cf90a47b20a46daf1d8affc75cf47edce53421b9
Flux Hallucinacion <fluxinated@hotmail.com>
2019-02-06 18:27:33 +0800

    added tab, sms_bloc fetch sms

--- lib/data/sms/sms_bloc.dart
+++ lib/data/sms/sms_bloc.dart
@@ -3,13 +3,23 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'sms_event.dart';
 import 'sms_state.dart';
+import 'package:sms/sms.dart';
+import 'dart:collection';
 
 class SMSBloc extends Bloc<SMSEvent, SMSState> {
+  SmsQuery query = SmsQuery();
   @override
   // TODO: implement initialState
   SMSState get initialState => SMSInitialState();
 
   @override
   Stream<SMSState> mapEventToState(
-      SMSState currentState, SMSEvent event) async* {}
+      SMSState currentState, SMSEvent event) async* {
+    print('event $event');
+    if (event is SMSFetchAllMessage) {
+      List<SmsMessage> messages = await query.getAllSms;
+      yield SMSFetchResult(data: UnmodifiableListView(messages));
+    }
+    // yield SMSInitialState();
+  }
 }
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,11 +1,71 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:sms_sender/screens/sms/index.dart';
+import 'package:sms_sender/data/sms/index.dart' as SMSBloc;
+import 'package:sms_sender/theme/style.dart';
 
 class Routes {
   Map<String, WidgetBuilder> routes = {
-    "/": (BuildContext context) => SMSList()
+    "/": (BuildContext context) => RootTab()
   };
 
-  Routes() {}
+  Routes() {
+    runApp(MyApp(routes: routes));
+  }
+}
+
+class RootTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return DefaultTabController(
+        length: 1,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('SMS Sender'),
+            centerTitle: true,
+            bottom: TabBar(
+              labelStyle: tabLabelStyle,
+              tabs: <Widget>[
+                Tab(
+                  key: UniqueKey(),
+                  text: 'Inbox',
+                )
+              ],
+            ),
+          ),
+          body: SMSList(),
+        ));
+  }
+}
+
+class MyApp extends StatefulWidget {
+  Map<String, WidgetBuilder> routes;
+
+  MyApp({this.routes});
+
+  @override
+  State<MyApp> createState() {
+    // TODO: implement createState
+    return MyAppState(routes: this.routes);
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  Map<String, WidgetBuilder> routes;
+
+  MyAppState({this.routes});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<SMSBloc.SMSBloc>(
+      bloc: SMSBloc.SMSBloc(),
+      child: MaterialApp(
+        title: 'SMS Sender App',
+        theme: appTheme,
+        routes: this.routes,
+        initialRoute: '/',
+      ),
+    );
+  }
 }
